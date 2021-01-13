40de096adf920d00f20e5277860498ede0faf7f8
smassarsky <seth.massarsky@gmail.com>
2021-01-13 13:58:33 -0500

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const HEART_SWITCHER = {
+  [EMPTY_HEART]: FULL_HEART,
+  [FULL_HEART]: EMPTY_HEART
+}
+
 // Your JavaScript code goes here!
 
+const errorModal = document.querySelector("#modal")
+const errorModalMessage = document.querySelector("#modal-message")
+const likeGlyphs = document.querySelectorAll(".like-glyph")
 
-
+for (glyph of likeGlyphs) {
+  glyph.addEventListener("click", e => {
+    
+    mimicServerCall()
+      .then(() => {
+        if (e.target.classList.contains("activated-heart")) {
+          e.target.innerText = EMPTY_HEART
+          e.target.classList.remove("activated-heart")
+        } else {
+          e.target.innerText = FULL_HEART
+          e.target.classList.add("activated-heart")
+        }
+      })
+      .catch(resp => {
+        errorModal.classList.remove("hidden")
+        errorModalMessage.innerText = resp
+        setTimeout(() => errorModal.classList.add("hidden"), 5000)
+      })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
